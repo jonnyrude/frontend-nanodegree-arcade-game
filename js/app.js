1e35f71a65b5dd603915335ca0a7bf6c95ddeead
JR <jrkrelix_01@hotmail.com>
2018-06-15 18:35:14 -0600

    Initiate Player!
    Player appears on canvas, and is moved with key-presses.

--- js/app.js
+++ js/app.js
@@ -24,13 +24,56 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function (x, y) {
+    this.x = x,
+    this.y = y,
+    this.sprite = 'images/char-boy.png'
+}
 
+
+Player.prototype.update = function (dt) {
+    // this.x += x;
+    // this.y += y;
+
+}
+
+Player.prototype.render = function () {
+ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function (keyCode) {
+    console.log(keyCode);
+    let adjustX = 0;
+    let adjustY = 0;
+    switch (keyCode) {
+        case 'up':
+            adjustY = -100;
+            break;
+        case 'left':
+            adjustX = -100;
+            break;
+        case 'right':
+            adjustX = 100;
+            break;
+        case 'down':
+            adjustY = 100;
+            break;
+        default:
+            ;
+    }
+
+    player.x += adjustX;
+    player.y += adjustY;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const player = new Player(200, 400);
 
+let enemy1 = new Enemy(100, 100);
 
+const allEnemies = [enemy1];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
